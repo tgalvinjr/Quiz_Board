21bd987853d6cefae185088f7af3fef24a1348cd
tgalvinjr <tgalvin.jr@gmail.com>
2019-04-03 23:13:03 +0300

    add UI logic on index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+//UI LOgic
+function checkanswer(){
+    var q1=document.questionForm.question1.value;
+    var q2=document.questionForm.question2.value;
+    var q3=document.questionForm.question2.value;
+    var q4=document.questionForm.question2.value;
+    var q4=document.questionForm.question2.value;
+    var markscounter=0;
+    
+}
+
+//BUSINESS LOGIC
